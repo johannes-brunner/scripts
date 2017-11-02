248577e5cb0c88a3362f1fddf8bab92185231616
Johannes Brunner <johannes.brunner@world-direct.at>
2017-11-02 10:50:34 +0100

    switch between wd and local. switch for upgrade

--- setup-dev-env.ps1
+++ setup-dev-env.ps1
@@ -1,39 +1,61 @@
-choco install visualstudiocode -y;
-choco install git -y;
-choco install docker -y;
-choco install docker-for-windows -y
-choco install sysinternals -y;
-choco install googlechrome -y;
-choco install everything -y;
-choco install 7zip.install -y;
-choco install putty.install -y;
-choco install keepass -y;
-choco install nuget.commandline -y;
-choco install winscp.install -y;
-choco install irfanview -y;
-choco install fiddler4 -y;
-choco install nmap --version 7.50 -y;
-choco install ilspy -y;
-choco install pdfxchangeeditor -y;
-choco install inkscape -y;
-choco install insomnia-rest-api-client -y;
-choco install firefox -y;
-choco install teamviewer -y;
-choco install skype -y;
-choco install gimp -y;
-choco install vlc -y;
-choco install hxd -y;
-choco install onenote -y;
-choco install jre8 -y;
-choco install dotnetcore -y;
-choco install dotnetcore-sdk -y;
-choco install dotnet4.6.2 -y;
-choco install microsoft-message-analyzer -y;
-choco install robo3t -y;
-choco install sql-server-management-studio -y;
-choco install office365proplus -y;
-choco install consolez -y;
-choco install chocolateygui -y;
-choco install jmeter -y;
-choco install choco install visualstudio2017community -y;
-#choco install visualstudio2017enterprise -y;  
+param (   
+    [switch]$upgrade = $false
+ )
+
+$packages = @(
+'visualstudiocode', 
+'git',
+'docker',
+'docker-for-windows',
+'sysinternals',
+'googlechrome',
+'everything',
+'7zip.install',
+'putty.install',
+'keepass',
+'nuget.commandline',
+'winscp.install',
+'irfanview',
+'fiddler4',
+'ilspy',
+'pdfxchangeeditor',
+'inkscape',
+'insomnia-rest-api-client',
+'firefox',
+'teamviewer',
+'skype',
+'gimp',
+'vlc',
+'hxd',
+'jre8',
+'dotnetcore',
+'dotnetcore-sdk',
+'dotnet4.6.2',
+'dotnet4.7',
+'microsoft-message-analyzer',
+'robo3t',
+'slack',
+'sql-server-management-studio',
+'consolez',
+'chocolateygui',
+'jmeter',
+'python',
+'nodejs.install'
+)
+
+if ($upgrade) {
+    choco upgrade $packages -y;
+} else {
+    choco install $packages -y;
+    choco install nmap --version 7.50 -y;
+}
+#I am in the world-direct network
+if (Test-Connection -ComputerName dc1office.world-direct.at -Count 1 -Quiet) {
+    #choco install visualstudio2017enterprise -y;  
+} else {
+    if ($update) {
+        choco upgrade visualstudio2017community -y;
+    } else {
+        choco install visualstudio2017community -y;
+    }
+}
